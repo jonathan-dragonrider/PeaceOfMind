71f02ae47e5e61825810133ada3375b1ec1ee086
jonphilk <jonphilk@gmail.com>
2020-09-28 08:58:18 -0500

    Add project files.

---
+++ PeaceOfMind.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PeaceOfMind.WebMVC.Startup))]
+namespace PeaceOfMind.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
