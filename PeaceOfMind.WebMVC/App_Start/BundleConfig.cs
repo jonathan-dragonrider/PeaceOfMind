334ab9011bbbf6a7e014f38418ba7315b203768f
jonphilk <jonphilk@gmail.com>
2020-10-07 12:58:21 -0500

    added FullCalendar files

--- PeaceOfMind.WebMVC/App_Start/BundleConfig.cs
+++ PeaceOfMind.WebMVC/App_Start/BundleConfig.cs
@@ -22,9 +22,13 @@ namespace PeaceOfMind.WebMVC
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/fullcalendar").Include(
+                      "~/FullCalendar/main.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/FullCalendar/main.css"));
         }
     }
 }
